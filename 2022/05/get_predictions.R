a48f7e266aa110b7dd4509b16c125d143f600c0f
Scott Flaska <scott.flaska@gmail.com>
2022-05-24 17:16:06 -0600

    try ensemble

--- 2022/05/get_predictions.R
+++ 2022/05/get_predictions.R
@@ -11,12 +11,35 @@ test_cleaned = clean_data(test)
 
 machine_state_model = readRDS("~/project/2022/05/outputs/machine_state_model.rds")
 
-test_cleaned$target = predict(machine_state_model, 
+machine_state_rf_model = readRDS("~/project/2022/05/outputs/machine_state_rf_model.rds")
+
+test_cleaned$xgb = predict(machine_state_model, 
                               newdata = test_cleaned, 
                               type = "prob") %>% 
   pull(one)
 
-submission_data = test_cleaned %>% 
+test_cleaned$rf = predict(machine_state_rf_model, 
+                          newdata = test_cleaned, 
+                          type = "prob") %>% 
+  pull(one)
+
+# test_cleaned %>% 
+#   slice_sample(n = 10000) %>% 
+#   ggplot(mapping = aes(x = rf,
+#                        y = xgb)) +
+#   geom_point()
+
+compare_data = test_cleaned %>% 
+  mutate(target = (xgb + rf)/2) %>% 
+  select(id, xgb, rf, target)
+
+# compare_data %>% 
+#   slice_sample(n= 10000) %>% 
+#   ggplot(mapping = aes(x = xgb,
+#                        y = target)) + 
+#   geom_point()
+
+submission_data = compare_data %>% 
   select(id, target)
 
 write_csv(x = submission_data,
