092373bc6a893c3ac22df187d8b0118a1d49c690
Scott Flaska <scott.flaska@gmail.com>
2022-05-24 08:53:29 -0600

    remove calibration step because it doesnt matter if rank is the same

--- 2022/05/get_predictions.R
+++ 2022/05/get_predictions.R
@@ -11,28 +11,13 @@ test_cleaned = clean_data(test)
 
 machine_state_model = readRDS("~/project/2022/05/outputs/machine_state_model.rds")
 
-cal_model = glm(obs ~ one, 
-                family = binomial,
-                data = machine_state_model$pred)
+test_cleaned$target = predict(machine_state_model, 
+                              newdata = test_cleaned, 
+                              type = "prob") %>% 
+  pull(one)
 
-machine_state_model_preds = predict(machine_state_model, 
-                                    newdata = test_cleaned, 
-                                    type = "prob")
-
-machine_state_model_preds$cal_pred = (1 - predict(cal_model, 
-                                             newdata = machine_state_model_preds,
-                                             type = "response"))
-
-# machine_state_model_preds %>% 
-#   slice_sample(n = 10000) %>% 
-#   ggplot(mapping = aes(x = one,
-#                        y = cal_pred)) + 
-#   geom_point()
-
-submission_data = test %>% 
-  select(id)
-
-submission_data$target = machine_state_model_preds$cal_pred
+submission_data = test_cleaned %>% 
+  select(id, target)
 
 write_csv(x = submission_data,
           file = "~/project/2022/05/outputs/submission_data.csv",
